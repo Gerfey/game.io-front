5e240f040f4303aab779911bfd467f2202b1bd80
Gerfey <sanjia@live.ru>
2022-11-21 10:50:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static('./dist'));
 app.use((req, res) => res.sendFile('index.html', {root: './dist'}));

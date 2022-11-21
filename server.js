823d885b6ae5cff5171720b4734e928bc4b30270
Gerfey <sanjia@live.ru>
2022-11-21 10:55:36 +0700

    попытки победить деплой

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./dist'));
 app.use((req, res) => res.sendFile('index.html', {root: './dist'}));
